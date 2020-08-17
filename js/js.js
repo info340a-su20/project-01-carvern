ab37fc95d5b571bf6895716d3b0ee62345436289
Nicole Carver <carvern@uw.edu>
2020-08-17 14:38:04 -0700

    updated popups

--- js/js.js
+++ js/js.js
@@ -40,7 +40,7 @@ $( function() {
   $.getJSON("data/map.geojson",function(data){
     let mapPoints = L.geoJson(data,{
       onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.name + "I am a standalone popup.");
+        layer.bindPopup("Name: " + feature.properties.name + "\nPrice" + feature.properties.price);
   }
 }).addTo(map);
 });
